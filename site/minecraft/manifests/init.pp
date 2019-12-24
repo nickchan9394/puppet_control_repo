cd1ada70b488eb271853b3b1f6f769bd46e9f198
Nick Chan <nickchan9394@live.cn>
2019-12-24 12:01:34 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
 class minecraft {
-  $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar'
+  $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
   $install_dir = '/opt/minecraft'
+}{
   file {'/opt/minecraft':
     ensure => directory,
   }
