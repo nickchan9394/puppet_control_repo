8a7e921c500ae04d52051b193edb269dd1e0c76d
Nick Chan <nickchan9394@live.cn>
2019-12-24 12:00:42 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,18 @@
 class minecraft {
+  $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar'
+  $install_dir = '/opt/minecraft'
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
